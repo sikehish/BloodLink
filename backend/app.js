c96fc9a9f85aa46179cb2280140d948248df035a
sikehish <hisham0502@gmail.com>
2025-04-18 23:08:24 +0530

    Initial commit for private testing

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const port = 3177;
+
+dotenv.config();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	next();
+  });
+  
+
+
+mongoose.connect("mongodb+srv://sparshmishra1:sparshmishra1@cluster0.m5ycaxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
+	console.log(e ? e : "Connected successfully to database");
+});
+
+app.use("/auth", require("./routers/authRouter"));
+app.use("/user", require("./routers/userRouter"));
+app.use("/bank", require("./routers/bankRouter"));
+app.use("/camps", require("./routers/campRouter"));
+
+app.listen(port, () =>
+	console.log(`Server running at http://localhost:${port}`)
+);
