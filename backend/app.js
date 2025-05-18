0354b7ed2349f953f930c6a0da5130f8d7c6f856
Hisham Akmal <hisham0502@gmail.com>
2025-05-18 20:21:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,22 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://bloodlink-frontend.s3-website.ap-south-1.amazonaws.com"
+];
+
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	next();
-  });
-  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+  }
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 
 
 mongoose.connect("mongodb+srv://sparshmishra1:sparshmishra1@cluster0.m5ycaxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
